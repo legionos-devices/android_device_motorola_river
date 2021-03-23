a55cbb50598c579a0767954a9c84cc2ae32f8d6a
Vitor Santos <78888873+VitorSSSouza@users.noreply.github.com>
2021-03-23 20:17:38 -0300

    river: Exclude Some Packages
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- RemovePacakges/Android.mk
+++ RemovePacakges/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gallery2 Jelly Email PhotoTable Photos Chrome Recorder Eleven ViaBrowser Music DuckDuckGo SimpleGallery Phonograph
+LOCAL_OVERRIDES_PACKAGES := Gallery2 Jelly Email PhotoTable Photos Chrome Velvet PrebuiltGmail Maps YouTube Drive mark.via.gp Turbo WellbeingPrebuilt FilesPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
